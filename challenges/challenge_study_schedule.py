f555829609082eddfbc4b2a794c03a8688272a87
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-09-20 15:13:09 -0300

    task 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    # iniciando projeto
-    raise NotImplementedError
+    if not target_time:
+        return None
+    count = 0
+    for start_period, end_period in permanence_period:
+        if not isinstance(start_period, int) \
+                or not isinstance(end_period, int):
+            return None
+        if start_period <= target_time <= end_period:
+            count += 1
+    return count
