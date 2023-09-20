e22b3819a73b8a718e28d6b461e58f4f8b559886
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-09-20 16:13:37 -0300

    task 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("hagnarok", "7")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(159, 2)
+
+    result = encrypt_message("hagnarok", 10)
+    assert result == "korangah"
+
+    result = encrypt_message("hagnarok", 5)
+    assert result == "angah_kor"
+
+    result = encrypt_message("hagnarok", 3)
+    assert result == "gah_koran"
