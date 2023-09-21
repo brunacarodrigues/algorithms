b81384ed78cfee456e0cf08f6e0dee7831ef7e7f
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-09-21 14:47:17 -0300

    task 04 refatorada

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,13 +1,23 @@
-def is_anagrams(first_string, second_string):
-    def count_characters(word):
-        char_count = {}
-        for char in word:
-            if char.isalpha():
-                char = char.lower()
-                char_count[char] = char_count.get(char, 0) + 1
-        return char_count
+# Refatorando por não poder utilizar funções prontas (sort, sorted, Counter)
 
-    first_count = count_characters(first_string)
-    second_count = count_characters(second_string)
+def quick_sort(string):
+    if len(string) <= 1:
+        return string
 
-    return first_count == second_count
+    mid = string[len(string) // 2]
+    first = [letter for letter in string if letter.lower() < mid.lower()]
+    middle = [letter for letter in string if letter.lower() == mid.lower()]
+    second = [letter for letter in string if letter.lower() > mid.lower()]
+
+    return quick_sort(first) + middle + quick_sort(second)
+
+
+def is_anagram(first_string, second_string):
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    first_sorted = ''.join(quick_sort(list(first_string)))
+    second_sorted = ''.join(quick_sort(list(second_string)))
+    if not first_string or not second_string:
+        return (first_sorted, second_sorted, False)
+
+    return (first_sorted, second_sorted, first_sorted == second_sorted)
