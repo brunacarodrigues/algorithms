dba53473653da62a471f2a2bc6dda595964b5978
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-09-21 11:27:49 -0300

    task 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 2:
+        return False
+
+    nums.sort()
+    seen = set()
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in seen:
+            return num
+        seen.add(num)
+
+    return False
