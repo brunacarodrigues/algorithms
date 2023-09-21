390e3302ff09d2bca3c1eaf66eaf5be89cf0624e
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-09-21 13:02:52 -0300

    task 04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,13 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_anagrams(first_string, second_string):
+    def count_characters(word):
+        char_count = {}
+        for char in word:
+            if char.isalpha():
+                char = char.lower()
+                char_count[char] = char_count.get(char, 0) + 1
+        return char_count
+
+    first_count = count_characters(first_string)
+    second_count = count_characters(second_string)
+
+    return first_count == second_count
